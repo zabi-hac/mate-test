c9538718748a94379291ac99040f26fb6b6ecce8
zabi-hac <74953733+zabi-hac@users.noreply.github.com>
2021-10-26 07:48:38 +0530

    Add files via upload

---
+++ src/boot.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from "react-bootstrap/Button";
+
+function App() {
+  return (
+    <div>
+      <Button variant="outline-success"  className="m-5 btn btn-sm">Click Me!</Button>
+      </div>
+      
+  );
+}
+
+export default App;
